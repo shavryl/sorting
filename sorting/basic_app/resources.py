9ed647f129c2747e3d8e6df4d0ffbb5bbffafcfe
Stanislav <gavrilchenko.stanislav@gmail.com>
2017-08-07 18:51:51 +0300

    Revert "Add files via upload"
    This reverts commit 04dc0ee03c2cd778ee8acc173241cb2adb781a2a.

--- sorting/basic_app/resources.py
+++
@@ -1,9 +0,0 @@
-from import_export import resources
-from .models import Profile
-
-
-
-class ProfileResource(resources.ModelResource):
-    class Meta:
-        model = Profile
-        exclude = ['user','id']
